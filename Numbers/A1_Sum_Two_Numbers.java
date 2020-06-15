fe0049e3da101cdccb813a59f17715239fd99de7
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-15 15:04:08 -0500

    Update A1_Sum_Two_Numbers.java

--- Numbers/A1_Sum_Two_Numbers.java
+++ Numbers/A1_Sum_Two_Numbers.java
@@ -1,15 +1,14 @@
 
 package Numbers;
-
 import java.util.Scanner;
 
 public class A1_Sum_Two_Numbers {
     public static Scanner scanner = new Scanner( System.in );
     public static void main(String[] args) {
-        System.out.println("Enter the first number: ");
-        double x = scanner.nextDouble();
-        System.out.println("Enter the second number: ");
-        double y = scanner.nextDouble();
+        System.out.println("Enter the first number: ");//Ask user for first input.
+        double x = scanner.nextDouble();//Takes first input from the user.
+        System.out.println("Enter the second number: ");//Ask user for second input.
+        double y = scanner.nextDouble();//Takes second input from the user.
 
         double sum = x + y;
         System.out.println("Sum of " + x + " and "+ y +" is "+ sum +".");

97c9c0a52fe431b1a04a43c3d730277c6eff14c8
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-21 17:10:54 -0500

    Update A1_Declaration.java

--- 2D-Array/A1_Declaration.java
+++ 2D-Array/A1_Declaration.java
@@ -1,5 +1,6 @@
 package Array2D;
-
+//This codes just demonstrates how 2D Array works.
+//It gives the idea how to input elements in 2D Array.
 public class A1_Declaration {
     public static void main(String[] args) {
             int[][] array = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};

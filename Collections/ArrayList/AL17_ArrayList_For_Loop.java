3896323c4da2468302b657f8457699b0211e9ed6
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-10 22:45:26 -0500

    Update AL17_ArrayList_For_Loop.java

--- Collections/ArrayList/AL17_ArrayList_For_Loop.java
+++ Collections/ArrayList/AL17_ArrayList_For_Loop.java
@@ -2,7 +2,7 @@ package Collections.ArrayList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
+//This program is a simple demonstration of using for loop in an arraylist.
 public class AL17_ArrayList_For_Loop {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>( Arrays.asList(1,3,4,5,6,7,8) );

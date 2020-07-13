3d6aae9f9106d49faf86f8c4e46f561022492adc
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-12 19:18:09 -0500

    Add files via upload

---
+++ Conversion/C1_Dec_To_Bin.java
@@ -0,0 +1,8 @@
+package Conversion;
+
+public class C1_Dec_To_Bin {
+    public static void main(String[] args) {
+        int n = 1234;
+        System.out.println("Binary: " + Integer.toBinaryString( n ));
+    }
+}
---
+++ Conversion/C2_Dec_To_Bin.java
@@ -0,0 +1,15 @@
+package Conversion;
+
+public class C2_Dec_To_Bin {
+    public static void main(String[] args) {
+        int dec = 1264;
+        String str = "";
+
+        while(dec > 0){
+            int rem = (int) dec % 2;
+            str = rem + str ;
+            dec = dec / 2;
+        }
+        System.out.println("Binary: " + str);
+    }
+}

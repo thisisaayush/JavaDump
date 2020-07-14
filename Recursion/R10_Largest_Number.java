11e98edb440d7194d066805205bab0b31313b1e1
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-13 21:57:56 -0500

    Update R10_Largest_Number.java

--- Recursion/R10_Largest_Number.java
+++ Recursion/R10_Largest_Number.java
@@ -1,14 +1,14 @@
 package Recursion;
-
 //Write the recursion code for smallest digit.
+//This program prints out the largest digit in a given number.
 public class R10_Largest_Number {
     static int max = Integer.MIN_VALUE;
     public static void main(String[] args) {
         int num = 373213;
         //smallestDigit( num );
-        System.out.println("Largest digit: " + smallestDigit( num ));
+        System.out.println("Largest digit: " + largestDigit( num ));
     }
-    public static int smallestDigit(int num){
+    public static int largestDigit(int num){
         int n = num % 10;
         if(n > max){
             max = n;
@@ -16,6 +16,6 @@ public class R10_Largest_Number {
        if(num == 0){
            return max;
        }
-       return smallestDigit( num / 10 );
+       return largestDigit( num / 10 );
     }
 }

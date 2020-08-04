eff3e094e42e829d625199cf4c0d0e26f471456f
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-03 22:24:02 -0500

    Update LL43_Iterate.java

--- Collections/LinkedList/LL43_Iterate.java
+++ Collections/LinkedList/LL43_Iterate.java
@@ -3,10 +3,10 @@ package Collections.LinkedList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
-
+//Demonstration on how to use LinkedList and iterator.
 public class LL43_Iterate {
     public static void main(String[] args) {
-        LinkedList<String> linkedList = new LinkedList<>( Arrays.asList("Tony","Hulk","Peter","Captain") );
+        LinkedList<String> linkedList = new LinkedList<>(Arrays.asList("Tony","Hulk","Peter","Captain") );
         Iterator itr = linkedList.iterator();
 
         while(itr.hasNext()){

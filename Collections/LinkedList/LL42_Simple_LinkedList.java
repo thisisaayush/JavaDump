5f2ed745304b544b8d0cc6e3e8e4676056387657
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-03 22:22:09 -0500

    Update LL42_Simple_LinkedList.java

--- Collections/LinkedList/LL42_Simple_LinkedList.java
+++ Collections/LinkedList/LL42_Simple_LinkedList.java
@@ -3,7 +3,7 @@ package Collections.LinkedList;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.LinkedList;
-
+//Simple way of creating LinkedList.
 public class LL42_Simple_LinkedList {
     public static void main(String[] args) {
         LinkedList<String> linkedList = new LinkedList<>( Arrays.asList("Ram","Shyam","Hari","Krishna","Shiva") );

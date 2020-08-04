b53adaf4cf1539ab05aeeadb3a4c3df45dc652cc
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-03 22:28:04 -0500

    Update LL52_Remove_Specific_Element.java

--- Collections/LinkedList/LL52_Remove_Specific_Element.java
+++ Collections/LinkedList/LL52_Remove_Specific_Element.java
@@ -2,7 +2,7 @@ package Collections.LinkedList;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-
+//Demonstration on removing elements from LinkedList.
 public class LL52_Remove_Specific_Element {
     public static void main(String[] args) {
         LinkedList<String> linkedList1 = new LinkedList<>( Arrays.asList("Albert Einstein", "Stephen Hwaking", "Richard Feymann","Max Plank","Max Well","Max Bohr","Neils Bohr","Ruterford","Thomson") );

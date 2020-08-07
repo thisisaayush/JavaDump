85facd0d12bfb43fda1a6794e36b9d80a84ad56e
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-07 15:38:41 -0500

    Update E3_Factorial_Using_Recursion.java

--- Numbers/E3_Factorial_Using_Recursion.java
+++ Numbers/E3_Factorial_Using_Recursion.java
@@ -1,7 +1,7 @@
 package Numbers;
 
 import java.util.Scanner;
-//This program prints out the factorial of a given positive number.
+//This program prints out the factorial of a given positive number. It uses the concept of recursion.
 public class E3_Factorial_Using_Recursion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner( System.in );

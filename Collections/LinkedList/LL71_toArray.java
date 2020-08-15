006bc5d9795c516c1b4ac58e0bf74f4b4ed83e94
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-15 08:19:08 -0500

    Update LL71_toArray.java

--- Collections/LinkedList/LL71_toArray.java
+++ Collections/LinkedList/LL71_toArray.java
@@ -2,7 +2,7 @@ package Collections.LinkedList;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-
+//The program demostrates the use of linkedlist and how to convert it into array.
 public class LL71_toArray {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList = new LinkedList<>( Arrays.asList(3,2,5,6,7) );

6ae6447e479cd88cc7b97fce7453fc4c9c76546a
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-15 08:19:43 -0500

    Update LL70_toString.java

--- Collections/LinkedList/LL70_toString.java
+++ Collections/LinkedList/LL70_toString.java
@@ -2,7 +2,7 @@ package Collections.LinkedList;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-
+//The prgram demonstrate the use of linkedlist and how to convert it into string.
 public class LL70_toString {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList = new LinkedList<>( Arrays.asList(2,3,4,6,7,9) );

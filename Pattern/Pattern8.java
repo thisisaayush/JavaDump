b2744584ccaffe41b8d79c2e001b65b0457ec435
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-25 19:50:49 -0500

    Update Pattern8.java

--- Pattern/Pattern8.java
+++ Pattern/Pattern8.java
@@ -1,5 +1,6 @@
 package Pattern;
-//The program prints out the alphabetical characters from A to I horizontally till the program reach the given rows and columns.
+//The program prints out the alphabetical characters from A to I horizontally 
+//repeating the same pattern untill the program reach the given rows and columns.
 public class Pattern8 {
     public static void main(String[] args) {
         for(int i = 1 ; i <= 10 ; i++){

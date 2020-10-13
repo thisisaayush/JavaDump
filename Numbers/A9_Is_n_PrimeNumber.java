5e3a9daf08841d868499c19ee9625ea2e6e2fc34
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-10-12 23:07:43 -0500

    Update A9_Is_n_PrimeNumber.java

--- Numbers/A9_Is_n_PrimeNumber.java
+++ Numbers/A9_Is_n_PrimeNumber.java
@@ -5,26 +5,28 @@
  * */
 package Numbers;
 
-import javax.print.attribute.standard.NumberUp;
 import java.util.Scanner;
 
 public class A9_Is_n_PrimeNumber {
-    private static Scanner scanner = new Scanner( System.in );
+    private static Scanner scanner = new Scanner( System.in);
 
     public static void main(String[] args) {
         boolean result = true;
         System.out.println("Enter number: ");
-        int num = scanner.nextInt();
+        int num = scanner.nextInt();//Takes input from user.
 
         for(int i = 2 ; i <= num / 2 ; i++){
-            if(num % i == 0) {
-                result = false;
+            if(num % i == 0) //Checks if the number is divisible by i  or not.
+            {
+                result = false;//if the above condition is met then it returns false result- which implies not a prime number
             }
         }
-        if(result){
+        if(result)//if result is true, it prints out num as a prime number.
+        {
             System.out.println( num + " is a prime number.");
         }
-        else{
+        else//otherwise, prints out as not a prime number.
+        {
             System.out.println(num + " is not a prime number.");
         }
     }

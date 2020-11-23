78af08e0ea626e1b5733bfcc32b68d817d41c4ab
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-11-22 20:50:17 -0600

    Add files via upload

---
+++ Strings/S1_initialization.java
@@ -0,0 +1,9 @@
+package Interview_Coding_Questions.Strings;
+
+public class S1_initialization {
+    public static void main(String[] args) {
+        String str;
+        str = "Hello Aayush!";
+        System.out.println(str);
+    }
+}

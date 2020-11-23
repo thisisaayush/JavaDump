303557286ed884cbfc6dfbbd41369f3b0f8f0bc0
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-11-22 20:50:44 -0600

    Add files via upload

---
+++ Strings/S2_initialization.java
@@ -0,0 +1,9 @@
+package Interview_Coding_Questions.Strings;
+
+public class S2_initialization {
+    public static void main(String[] args) {
+        char[] ch = {'H','e','l','l','o',' ','A','a','y','u','s','h','!'};
+        String str = new String(ch);
+        System.out.println(str);
+    }
+}

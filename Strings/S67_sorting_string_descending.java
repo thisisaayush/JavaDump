39444ca3138247bbafcdc03d7013a94599d163a3
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-12-23 16:56:28 -0600

    Add files via upload

---
+++ Strings/S67_sorting_string_descending.java
@@ -0,0 +1,19 @@
+package Interview_Coding_Questions.Strings;
+
+public class S67_sorting_string_descending {
+    public static void main(String[] args) {
+        String str = "Hello Aayush!";
+        char[] array = str.toCharArray();
+
+        for(int i = 0 ; i < array.length ; i++){
+            for(int j = 0 ; j < array.length - 1 ; j++){
+                if(array[j] < array[j + 1]){
+                    char temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("After sorting: " + new String(array));
+    }
+}
